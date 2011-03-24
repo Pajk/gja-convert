80689c6a4dfee80ccc13a36964b6a3ff61631a2c
pajkycz <pajkycz@1f729fb8-72f3-6b02-0148-79d6c8f41d11>
2011-03-24 12:30:46 +0000

    nastaveni systemoveho vzhledu

--- gja-convert/src/cz/vutbr/fit/convert/Convert.java
+++ gja-convert/src/cz/vutbr/fit/convert/Convert.java
@@ -2,6 +2,7 @@ package cz.vutbr.fit.convert;
 import cz.vutbr.fit.convert.gui.*;
 import cz.vutbr.fit.convert.settings.Config;
 import cz.vutbr.fit.convert.settings.Lang;
+import javax.swing.UIManager;
 /**
  * Main class
  * Hlavni trida aplikace
@@ -11,13 +12,21 @@ import cz.vutbr.fit.convert.settings.Lang;
 public class Convert {
 
 	/**
-         * Konstruktor - nacte se konfigurace a prislusne jazykove popisky, pak hlavni okno aplikace
+     * Konstruktor - nacte se konfigurace a prislusne jazykove popisky, pak hlavni okno aplikace
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Config.init();
 		Lang.init();
-		new MainWindow();
+        
+        // nastaveni systemoveho vzhledu
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            // nepovedlo se, nevadi, vzhled bude multiplatformni
+        }
+        
+        new MainWindow();
 	}
 
 }
