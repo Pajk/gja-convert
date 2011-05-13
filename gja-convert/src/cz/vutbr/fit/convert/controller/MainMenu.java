9b99bd44e10c0b9d4ac66dd4463d6ac219c55df4
pajkycz <pajkycz@1f729fb8-72f3-6b02-0148-79d6c8f41d11>
2011-05-13 08:42:51 +0000

    spusteni pisnicky po dvojkliku

--- gja-convert/src/cz/vutbr/fit/convert/controller/MainMenu.java
+++ gja-convert/src/cz/vutbr/fit/convert/controller/MainMenu.java
@@ -1,24 +1,30 @@
 package cz.vutbr.fit.convert.controller;
 
+//~--- non-JDK imports --------------------------------------------------------
+
 import cz.vutbr.fit.convert.Convert;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-
 import cz.vutbr.fit.convert.TaskManager;
 import cz.vutbr.fit.convert.gui.About;
 import cz.vutbr.fit.convert.gui.MainWindow;
 import cz.vutbr.fit.convert.gui.SettingsWindow;
 import cz.vutbr.fit.convert.settings.Lang;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import java.io.IOException;
+
 import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.swing.JFileChooser;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 
@@ -29,67 +35,78 @@ import javax.swing.filechooser.FileFilter;
  * @author xizakt00
  */
 public final class MainMenu extends JMenuBar {
-
     private static final long serialVersionUID = -6886186422189033178L;
-    /**
-     * Menu pro Konverzi do různých formátů
-     */
-    private static JMenu convert = new JMenu();
+
     /**
      * Menu pro Nápovědu, O aplikaci, Nastavení
      */
     private static JMenu others = new JMenu();
+
     /**
      * Tlacitko pro help
      */
     private static JMenuItem help = new JMenuItem();
+
+    /**
+     * Menu pro Konverzi do různých formátů
+     */
+    private static JMenu convert = new JMenu();
+
     /**
      * Tlacitko pro konverzi do formatu OGG
      */
     private static JMenuItem toOGG = new JMenuItem();
+
     /**
      * Tlacitko pro konverzi do formatu FLAC
      */
-    private static JMenuItem toFLAC = new JMenuItem();
+    private static JMenuItem      toFLAC         = new JMenuItem();
+    private static SettingsWindow settingsWindow = new SettingsWindow();
+
     /**
      * Tlacitko pro zobrazeni okna s nastavenim
      */
-    private static JMenuItem settings = new JMenuItem();
+    private static JMenuItem settings    = new JMenuItem();
+    private static JMenuItem quit        = new JMenuItem();
+    private static About     aboutDialog = new About();
+
     /**
      * Tlacitko pro zobrazeni okna O aplikaci
      */
     private static JMenuItem about = new JMenuItem();
-
-
-    private static JMenuItem quit = new JMenuItem();
-
-    private static SettingsWindow settingsWindow = new SettingsWindow();
-    private static About aboutDialog = new About();
 
     /**
      * Konstruktor
      */
     public MainMenu() {
         setNames();
-
         convert.setName("file_menu");
 
-        //toOGG.setMnemonic(KeyEvent.VK_O);
+        // toOGG.setMnemonic(KeyEvent.VK_O);
         toOGG.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.ALT_MASK));
         toOGG.addActionListener(new ActionListener() {
-
             @SuppressWarnings("deprecation")
             public void actionPerformed(ActionEvent e) {
-                /*FileDialog fd=new FileDialog(new Frame(),Lang.get("filedialog_header")+" OGG",FileDialog.LOAD);
-                fd.setLocation(50, 50);
-                fd.show();
-                if (fd.getFile() != null) TaskManager.addTask(fd.getDirectory() + fd.getFile(),"OGG");*/
+
+                /*
+                 * FileDialog fd=new FileDialog(new Frame(),Lang.get("filedialog_header")+" OGG",FileDialog.LOAD);
+                 * fd.setLocation(50, 50);
+                 * fd.show();
+                 * if (fd.getFile() != null) TaskManager.addTask(fd.getDirectory() + fd.getFile(),"OGG");
+                 */
                 JFileChooser chooser = new JFileChooser();
+
                 chooser.setName("file_chooser");
-                FileFilter filter = new ExtensionFileFilter("Music", new String[]{"CUE", "FLAC", "APE", "M4A", ".WV", "MP3", "MOD", "IT", "XM", "WAV", "S3M"});
+
+                FileFilter filter = new ExtensionFileFilter("Music", new String[] {
+                    "CUE", "FLAC", "APE", "M4A", ".WV", "MP3", "MOD", "IT", "XM", "WAV", "S3M"
+                });
+
                 chooser.setFileFilter(filter);
                 chooser.setDialogTitle(Lang.get("filedialog_header") + " OGG");
+
                 int returnVal1 = chooser.showOpenDialog(MainWindow.ogg_frame);
+
                 if (returnVal1 == JFileChooser.APPROVE_OPTION) {
                     TaskManager.addTask(chooser.getSelectedFile().getPath(), "OGG");
                 }
@@ -97,24 +114,28 @@ public final class MainMenu extends JMenuBar {
         });
         toOGG.setName("file_ogg_menuitem");
         convert.add(toOGG);
-        //toFLAC.setMnemonic(KeyEvent.VK_F);
+
+        // toFLAC.setMnemonic(KeyEvent.VK_F);
         toFLAC.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.ALT_MASK));
         toFLAC.addActionListener(new ActionListener() {
-
             @SuppressWarnings("deprecation")
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
-                FileFilter filter = new ExtensionFileFilter("Music", new String[]{"CUE", "OGG", "APE", "M4A", ".WV", "MP3", "MOD", "IT", "XM", "WAV", "S3M"});
+                FileFilter   filter  = new ExtensionFileFilter("Music", new String[] {
+                    "CUE", "OGG", "APE", "M4A", ".WV", "MP3", "MOD", "IT", "XM", "WAV", "S3M"
+                });
+
                 chooser.setFileFilter(filter);
                 chooser.setDialogTitle(Lang.get("filedialog_header") + " FLAC");
+
                 int returnVal1 = chooser.showOpenDialog(MainWindow.flac_frame);
+
                 if (returnVal1 == JFileChooser.APPROVE_OPTION) {
                     TaskManager.addTask(chooser.getSelectedFile().getPath(), "FLAC");
                 }
             }
         });
         convert.add(toFLAC);
-
         quit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.ALT_MASK));
         quit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -123,12 +144,11 @@ public final class MainMenu extends JMenuBar {
         });
         convert.add(quit);
 
-        //settings.setMnemonic(KeyEvent.VK_S);
+        // settings.setMnemonic(KeyEvent.VK_S);
         settings.setName("settings_window_menuitem");
         settingsWindow.setName("settings_window_dialog");
         settings.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.ALT_MASK));
         settings.addActionListener(new ActionListener() {
-
             public void actionPerformed(ActionEvent e) {
                 settingsWindow.setVisible(true);
             }
@@ -136,20 +156,18 @@ public final class MainMenu extends JMenuBar {
         others.add(settings);
         help.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.ALT_MASK));
         help.addActionListener(new ActionListener() {
-
             public void actionPerformed(ActionEvent e) {
                 try {
                     Desktop.getDesktop().browse(new URI("http://code.google.com/p/gja-convert/wiki/usage"));
-                } catch (IOException ex) {
-                } catch (URISyntaxException ex) {
-                }
+                } catch (IOException ex) {}
+                catch (URISyntaxException ex) {}
             }
         });
         others.add(help);
-        //about.setMnemonic(KeyEvent.VK_A);
+
+        // about.setMnemonic(KeyEvent.VK_A);
         about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.ALT_MASK));
         about.addActionListener(new ActionListener() {
-
             public void actionPerformed(ActionEvent e) {
                 aboutDialog.setVisible(true);
             }
--- gja-convert/src/cz/vutbr/fit/convert/controller/Panel.java
+++ gja-convert/src/cz/vutbr/fit/convert/controller/Panel.java
@@ -1,8 +1,13 @@
 package cz.vutbr.fit.convert.controller;
 
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import java.util.List;
+//~--- non-JDK imports --------------------------------------------------------
+
+import cz.vutbr.fit.convert.TaskManager;
+import cz.vutbr.fit.convert.settings.Config;
+import cz.vutbr.fit.convert.settings.Lang;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.awt.Frame;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
@@ -15,17 +20,22 @@ import java.awt.dnd.DropTargetListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import javax.swing.JLabel;
 
-import cz.vutbr.fit.convert.TaskManager;
-import cz.vutbr.fit.convert.settings.Config;
-import cz.vutbr.fit.convert.settings.Lang;
 import java.io.File;
+
 import java.net.URI;
+
+import java.util.List;
 import java.util.StringTokenizer;
+
+import javax.imageio.ImageIO;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.filechooser.FileFilter;
+
 /**
  * Panel class
  * Panel naslouchajici pro drop
@@ -33,79 +43,89 @@ import javax.swing.filechooser.FileFilter;
  * @author xizakt00
  */
 public class Panel extends JPanel implements DropTargetListener {
-
     private static final long serialVersionUID = 5420077495778750284L;
+
+    /**
+     * Obrazek formatu s tooltipem
+     */
+    private JLabel picLabel = null;
+
     /**
      * Jmeno panelu - OGG nebo FLAC
      */
     private String panelName;
-    /**
-     * Obrazek formatu s tooltipem
-     */
-    private JLabel picLabel=null;
+
     /**
      * Konstruktor
      * @param path jmeno souboru se zobrazovanym obrazkem
      * @param panelNam jmeno panelu - OGG nebo FLAC
      */
     public Panel(String path, String panelNam) {
-        @SuppressWarnings("unused")
-        DropTarget dt;
-        BufferedImage myPicture;
+        @SuppressWarnings("unused") DropTarget dt;
+        BufferedImage                          myPicture;
+
         this.panelName = panelNam;
+
         try {
             myPicture = ImageIO.read(Config.class.getResource(path));
-            picLabel = new JLabel(new ImageIcon(myPicture));
+            picLabel  = new JLabel(new ImageIcon(myPicture));
         } catch (Exception e) {
             picLabel = new JLabel(Lang.get("file") + " " + path + " " + Lang.get("not_found"));
         }
+
         actualize();
         dt = new DropTarget(picLabel, this);
-
         picLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 2) {
                     JFileChooser chooser = new JFileChooser();
-                FileFilter filter = new ExtensionFileFilter("Music", new String[] { "CUE", "OGG","FLAC","APE","M4A",".WV","MP3","MOD","IT","XM","WAV","S3M"});
-                chooser.setFileFilter(filter);
-                chooser.setDialogTitle(Lang.get("filedialog_header")+" "+panelName);
-                int returnVal1 = chooser.showOpenDialog(new Frame());
-                if(returnVal1 == JFileChooser.APPROVE_OPTION) {
-                   TaskManager.addTask(chooser.getSelectedFile().getPath(),panelName);
-                }
+                    FileFilter   filter  = new ExtensionFileFilter("Music", new String[] {
+                        "CUE", "OGG", "FLAC", "APE", "M4A", ".WV", "MP3", "MOD", "IT", "XM", "WAV", "S3M"
+                    });
+
+                    chooser.setFileFilter(filter);
+                    chooser.setDialogTitle(Lang.get("filedialog_header") + " " + panelName);
+
+                    int returnVal1 = chooser.showOpenDialog(new Frame());
+
+                    if (returnVal1 == JFileChooser.APPROVE_OPTION) {
+                        TaskManager.addTask(chooser.getSelectedFile().getPath(), panelName);
+                    }
                 }
             }
         });
         this.add(picLabel);
     }
-    public void actualize(){
-        picLabel.setToolTipText(Lang.get("panel_tooltip")+" "+ panelName);
+
+    public void actualize() {
+        picLabel.setToolTipText(Lang.get("panel_tooltip") + " " + panelName);
     }
+
     /**
      * Nedela nic
      * @param dtde
      */
-    public void dragEnter(DropTargetDragEvent dtde) {
-    }
+    public void dragEnter(DropTargetDragEvent dtde) {}
+
     /**
      * Nedela nic
      * @param dte
      */
-    public void dragExit(DropTargetEvent dte) {
-    }
+    public void dragExit(DropTargetEvent dte) {}
+
     /**
      * Nedela nic
      * @param dtde
      */
-    public void dragOver(DropTargetDragEvent dtde) {
-    }
+    public void dragOver(DropTargetDragEvent dtde) {}
+
     /**
      * Nedela nic
      * @param dtde
      */
-    public void dropActionChanged(DropTargetDragEvent dtde) {
-    }
+    public void dropActionChanged(DropTargetDragEvent dtde) {}
+
     /**
      * Funkce po akci drag analyzuje prijata data a pripadne prida novou ulohu ke zpracovani
      * Funguje pod MacOS X a Windows
@@ -115,39 +135,52 @@ public class Panel extends JPanel implements DropTargetListener {
     @SuppressWarnings("rawtypes")
     public void drop(DropTargetDropEvent dtde) {
         try {
-            Transferable tr = dtde.getTransferable();
+            Transferable tr      = dtde.getTransferable();
             DataFlavor[] flavors = tr.getTransferDataFlavors();
-            DataFlavor linux = new DataFlavor("text/uri-list;class=java.lang.String");
+            DataFlavor   linux   = new DataFlavor("text/uri-list;class=java.lang.String");
+
             for (int i = 0; i < flavors.length; i++) {
                 if (flavors[i].isFlavorJavaFileListType()) {
                     dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+
                     List list;
-                        list = (List) tr.getTransferData(flavors[i]);
-                        for (int j = 0; j < list.size(); j++) {
-                            if (list.get(j) != "") {
-                                TaskManager.addTask(list.get(j).toString(), panelName);
-                            }
+
+                    list = (List) tr.getTransferData(flavors[i]);
+
+                    for (int j = 0; j < list.size(); j++) {
+                        if (list.get(j) != "") {
+                            TaskManager.addTask(list.get(j).toString(), panelName);
                         }
-                    
+                    }
+
                     dtde.dropComplete(true);
+
                     return;
                 }
+
                 if (flavors[i].equals(linux)) {
                     dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+
                     String data = (String) tr.getTransferData(linux);
-                    for (StringTokenizer st = new StringTokenizer(data, "\r\n"); st.hasMoreTokens();) {
+
+                    for (StringTokenizer st = new StringTokenizer(data, "\r\n"); st.hasMoreTokens(); ) {
                         String token = st.nextToken().trim();
+
                         if (token.startsWith("#") || token.isEmpty()) {
                             continue;
                         }
-                        //TODO on linux systems - bad localization characters
-                        //System.out.println(new String(token.getBytes(System.getProperty("file.encoding"))));
+
+                        // TODO on linux systems - bad localization characters
+                        // System.out.println(new String(token.getBytes(System.getProperty("file.encoding"))));
                         System.out.println(token);
+
                         File file = new File(new URI(token));
-                        
+
                         TaskManager.addTask(file.getAbsolutePath(), panelName);
                     }
+
                     dtde.dropComplete(true);
+
                     return;
                 }
             }
